f5fb4d667e35876191c52f8939d7e06d9c0719d4
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-04-11 05:12:03 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,9 +4,10 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git 'git@github.com:https-github-com-Sedkibani/nxtya.git'
-                git branch: 'main', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
-                git credentialsId: 'Sedkibani', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
+                 git branch: 'main', credentialsId: 'Sedkibani', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
+                //git 'git@github.com:https-github-com-Sedkibani/nxtya.git'
+               // git branch: 'main', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
+               // git credentialsId: 'Sedkibani', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
             }
         }
 
