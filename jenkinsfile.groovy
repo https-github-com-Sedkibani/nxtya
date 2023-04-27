00e845bd0231e146b146114aebef8de82ab13f17
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-04-27 22:13:08 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,75 +1,75 @@
-agent any
-
-stages {
-    stage('Checkout') {
-        steps {  
-            git branch: 'main', credentialsId: 'Sedkibani   ', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
-            }
-    }
+pipeline {
+    agent any
     
-
-    stage('Build') {
-        steps {
-            sh  'docker build -t nxtya:1.0 -f docker/Dockerfile .'
-        }
-    }
-    
-       stage('Docker Login') {
-        steps {
-            // Login to Docker Hub with username and password
-            withCredentials([
-                usernamePassword(credentialsId: 'Sedkibani', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')
-            ]) {
-                sh 'docker login -u banisedki -p heisenberg.1889'
+    stages {
+        stage('Checkout') {
+            steps {  
+                git branch: 'main', credentialsId: 'Sedkibani', url: 'git@github.com:https-github-com-Sedkibani/nxtya.git'
             }
         }
-    }
-    
-    stage('Push to Docker Hub') {
-        steps {
-            sh 'docker push banisedki/nxtya'
+        
+        stage('Build') {
+            steps {
+                sh  'docker build -t nxtya:1.0 -f docker/Dockerfile .'
+            }
+        }
+        
+        stage('Docker Login') {
+            steps {
+                // Login to Docker Hub with username and password
+                withCredentials([
+                    usernamePassword(credentialsId: 'Sedkibani', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')
+                ]) {
+                    sh 'docker login -u banisedki -p heisenberg.1889'
+                }
+            }
+        }
+        
+        stage('Push to Docker Hub') {
+            steps {
+                sh 'docker push banisedki/nxtya'
+            }
+        }
+
+        /* stage('Unit Test') {
+            steps {
+                sh 'docker run --rm nxtya:1.0 vendor/bin/phpunit'
+            }
+        }*/
+
+        /*stage('Code Quality') {
+            steps {
+                sh 'docker run --rm nxtya:1.0 vendor/bin/phpstan analyze'
+                // Additional commands for other code quality tools like SonarQube
+            }
+        }*/
+
+        stage('Deploy') {
+            steps {
+                // Use Ansible playbook to deploy to DigitalOcean server
+                ansiblePlaybook(
+                    playbook: '/var/lib/jenkins/workspace/nxtya/ansible.yml',
+                    inventory: '/var/lib/jenkins/workspace/nxtya/inventory.ini',
+                    extras: "-e 'docker_image=nxtya:1.0'"
+                )
+            }
         }
     }
 
-    /* stage('Unit Test') {
-        steps {
-            sh 'docker run --rm nxtya:1.0 vendor/bin/phpunit'
-        }
-    }*/
+    // Additional stages for monitoring and security checks
 
-    /*stage('Code Quality') {
-        steps {
-            sh 'docker run --rm nxtya:1.0 vendor/bin/phpstan analyze'
-            // Additional commands for other code quality tools like SonarQube
-        }
-    }*/
-
-    stage('Deploy') {
-        steps {
-            // Use Ansible playbook to deploy to DigitalOcean server
-            ansiblePlaybook(
-                playbook: '/var/lib/jenkins/workspace/nxtya/ansible.yml',
-                inventory: '/var/lib/jenkins/workspace/nxtya/inventory.ini',
-                extras: "-e 'docker_image=nxtya:1.0'"
+    post {
+        always {
+            // Set up email notifications
+            emailext (
+                to: 'sedki99bani@gmail.com',
+                subject: 'Pipeline Status - ${currentBuild.result}',
+                body: """<p>Pipeline Status: ${currentBuild.result}</p>
+                         <p>Build URL: ${env.BUILD_URL}</p>""",
+                recipientProviders: [[$class: 'RequesterRecipientProvider']],
+                replyTo: 'jenkins@yourdomain.com',
+                mimeType: 'text/html'
             )
         }
     }
 }
-
-// Additional stages for monitoring and security checks
-
-post {
-    always {
-        // Set up email notifications
-        emailext (
-            to: 'sedki99bani@gmail.com',
-            subject: 'Pipeline Status - ${currentBuild.result}',
-            body: """<p>Pipeline Status: ${currentBuild.result}</p>
-                     <p>Build URL: ${env.BUILD_URL}</p>""",
-            recipientProviders: [[$class: 'RequesterRecipientProvider']],
-            replyTo: 'jenkins@yourdomain.com',
-            mimeType: 'text/html'
-        )
-    }
-}
-
